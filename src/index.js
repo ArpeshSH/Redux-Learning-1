54d75fe8c045e13823fd4f614b664961f9e6573e
ArpeshSH <ash@Ashs-MacBook-Pro.local>
2021-07-15 21:35:56 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+
+
+// import {createStore } from 'redux';
+
+// //Store -> GLOBalized State
+
+// //Action Increment
+// const increment  =() =>{
+//   return {
+//     type: "INCREMENT"
+//   }
+// }
+
+// const decrement  =() =>{
+//   return {
+//     type: "DECEREMENT"
+//   }
+// }
+
+// //Reducer
+
+// const counter = (state = 0 , action) =>{
+//   switch(action.type){
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default :
+//         return state;
+//   }
+// }
+
+// let store = createStore(counter)
+
+// //Display it in the console
+
+// store.subscribe(() => console.log(store.getState()))
+
+// //Dispatch
+
+// store.dispatch(increment())
+
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './reducers/index'
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+<Provider store ={store}>
+
+<App />
+</Provider>
+,document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  
